4a6f4d132eca95e88314732b4c2bed660c2cd263
martinsh <artmartinsh@gmail.com>
2021-05-26 09:33:50 +0300

    adding 3min vid

--- videoshader.js
+++ videoshader.js
@@ -130,7 +130,7 @@ AFRAME.registerShader('my-custom', {
           // v = (1/2)^(1/2) = 2^(-1/2) = INVSQRT2. Using this we can
           // linearly map h = [0,2r] -> x = [0,3v]. Why 3v is to cover
           // approximately 100% of the distribution. 
-          return vec3(exp(-sqr((3.0*INVSQRT2/1.5) * (h/r))) / (light.attenuation + sqr(d)*sr),vec2(PlUV.x/light.size.x,PlUV.y/light.size.y)*0.5+0.5);
+          return vec3(exp(-sqr((3.0*INVSQRT2/1.0) * (h/r))) / (light.attenuation + sqr(d)*sr),vec2(PlUV.x/light.size.x,PlUV.y/light.size.y)*0.5+0.5);
       }
 
       vec3 RectLight_shade(in RectLight light, in Material material, in sampler2D src ,in vec3 P, in vec3 N, in vec3 R, float NoR)
